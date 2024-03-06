e3cc8c29642fd06d2251649156998d40a0daeffa
Raj Das <rajdas.2001@gmail.com>
2024-03-07 01:20:22 +0530

    Version 2

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import google.generativeai as genai
 import streamlit as st
-
+from PIL import Image
 #model
 genai.configure(api_key="YOUR_API_KEY")
 model = genai.GenerativeModel('gemini-pro')
 
 #streamlit app
-st.title("How can I help you today?")
 
+
+# Loading Image using PIL
+im = Image.open('chat.jpg')
+# Adding Image to web app
+st.set_page_config(page_title="RxBot - Chatbot", page_icon = im)
+
+st.title("How can I help you today?")
 main_placeholder = st.empty()
 query = main_placeholder.text_input("", placeholder="Type here")
 main_placeholder_again = st.empty()
